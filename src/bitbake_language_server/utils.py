e8fa3513d6f0aa91d6cb0c5a01050cb4be53ed99
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-12-23 13:04:06 +0800

    :sparkles: Add Goto Definition

---
+++ src/bitbake_language_server/utils.py
@@ -0,0 +1,19 @@
+r"""Utils
+=========
+"""
+from oelint_parser.cls_item import Item
+from pygls.uris import from_fs_path
+
+
+def render_document(item: Item) -> str:
+    r"""Render document.
+
+    :param item:
+    :type item: Item
+    :rtype: str
+    """
+    return f"""<{from_fs_path(item.Origin)}>:{item.Line}:
+```bitbake
+{item.Raw.strip()}
+```
+"""
